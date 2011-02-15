3c65a9c8ea74a7d4f3ca29ca2b8f99205a7dba8b
melihbirim@gmail.com <melihbirim@gmail.com@2b36afa4-575e-11de-b59c-372aa07dcf7f>
2011-02-15 11:32:20 +0000

    removed

--- trunk/src/test/java/org/zoda/util/TestPeriodicDate.java
+++ trunk/src/test/java/org/zoda/util/TestPeriodicDate.java
@@ -8,7 +8,7 @@ import org.zoda.dao.impl.PeriodicDao;
 import org.zoda.model.*;
 
 /**
- * User: Melih Birim -- melih.birim@alcatel-lucent.com
+ * User: Melih Birim 
  * Date: Jun 19, 2009
  * Time: 5:14:04 PM
  * To change this template use File | Settings | File Templates.
